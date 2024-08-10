30299a42ab32684836fe1dd7f489508a57ba698c
kassan0 <abdallahkassan0@gmail.com>
2024-08-10 23:06:52 +0200

    updating paths of the templates for organazing the code

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,7 @@ from .views import signup_view, home_view, contact_view, article_list, article_d
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('contact/', contact_view, name='contact'),
     path('', home_view, name='home'),
