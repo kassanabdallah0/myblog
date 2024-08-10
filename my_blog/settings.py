59723c6e8a7900911ab48f44eba33e65a531bd7c
kassan0 <abdallahkassan0@gmail.com>
2024-08-10 16:02:49 +0200

    build login signup and logout pages with the necessary authentification for the user + testing the contact functionality

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -149,10 +149,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "blog/static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+LOGIN_URL = 'api/login'
+LOGIN_REDIRECT_URL = 'home'
