a6e2e5e949de98788d68de32b5569be442241a96
kassan0 <abdallahkassan0@gmail.com>
2024-08-11 18:51:51 +0200

    adding style using bootstrap and custom css

--- blog/forms.py
+++ blog/forms.py
@@ -3,26 +3,47 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Article, Event
 
-
 # Signup Form :
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text='Required')
-
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ['username', 'password1', 'password2']
+        
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={"class": "form-control"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"class": "form-control"})
+    )
 
-# Contact Form : 
 class ContactForm(forms.Form):
-    email = forms.EmailField(max_length=255, widget=forms.EmailInput(attrs={'placeholder': 'Your email address'}))
-    request_type = forms.ChoiceField(choices=[('contact', 'Contact'), ('bug', 'Bug Report')], label='Type of Request')
-    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your message'}))
+    email = forms.EmailField(
+        label="Your Email",
+        widget=forms.EmailInput(attrs={"class": "form-control"})
+    )
+    request_type = forms.ChoiceField(
+        choices=[('contact', 'Contact'), ('bug', 'Bug Report')],
+        label="Request Type",
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
+    message = forms.CharField(
+        label="Your Message",
+        widget=forms.Textarea(attrs={"class": "form-control"})
+    )
 
 # Article Form : 
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'content', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+        }
 
 # Event Form : 
 class EventForm(forms.ModelForm):
@@ -30,6 +51,9 @@ class EventForm(forms.ModelForm):
         model = Event
         fields = ['title', 'content', 'start_date', 'end_date', 'image']
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
