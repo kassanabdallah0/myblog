2090b5376e73e708821672b2453fde6e16f76f0e
kassan0 <abdallahkassan0@gmail.com>
2024-08-11 11:58:06 +0200

    fixing bug in delete handling in event and articles by overwriting the models specificaly the save and delete functions

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,62 @@
 from django.db import models
+import os
+from django.conf import settings
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    image = models.ImageField(upload_to='articles/')
 
     def __str__(self):
         return self.title
-    
+
+    def save(self, *args, **kwargs):
+        try:
+            # Check if the object already exists in the database
+            this = Article.objects.get(id=self.id)
+            if this.image != self.image:
+                # If the image is different, delete the old one
+                if os.path.isfile(this.image.path):
+                    os.remove(this.image.path)
+        except:
+            pass  # This is a new object, so there's no old image to delete
+        
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs)
+
+
 class Event(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    start_date = models.DateTimeField(null=True, blank=True)
-    end_date = models.DateTimeField(null=True, blank=True)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField(blank=True, null=True)
     image = models.ImageField(upload_to='events/')
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        try:
+            # Check if the object already exists in the database
+            this = Event.objects.get(id=self.id)
+            if this.image != self.image:
+                # If the image is different, delete the old one
+                if os.path.isfile(this.image.path):
+                    os.remove(this.image.path)
+        except:
+            pass  # This is a new object, so there's no old image to delete
+        
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.image:
+            if os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+        super().delete(*args, **kwargs)
